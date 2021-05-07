68310daddb6649322e0197e7e64dce13f8769684
vsvai <vsvaibhavsingh5@gmail.com>
2021-05-07 22:23:55 +0530

    pushing testing

--- main.fb6bbcaf.js
+++ main.fb6bbcaf.js
@@ -197,6 +197,49 @@ console.log("hover");
 require("../scss/main.scss");
 
 require("./hover");
+
+console.log("main.js running");
+var btn = document.getElementById("test");
+console.log(btn);
+
+if (btn) {
+  btn.addEventListener("click", visitingcard());
+}
+
+var open = 0;
+
+function visitingcard() {
+  console.log('working card');
+  var visit = document.getElementById("visit");
+  visit.innerHTML('<img src="./assests/home1.jpeg"/>');
+}
+
+var textarea = document.getElementById('comment');
+
+var changeCursor = function changeCursor(e) {
+  var value = e.target.value;
+  var newClass = getRangeColor(e.target.maxLength, value.length);
+  e.target.classList.remove('good', 'warning', 'danger');
+  e.target.classList.add(newClass);
+};
+
+var getRangeColor = function getRangeColor(maxLen, inputLen) {
+  var range = maxLen / 3;
+
+  if (between(inputLen, 0, range)) {
+    return 'good';
+  } else if (between(inputLen, range, range * 2)) {
+    return 'warning';
+  } else {
+    return 'danger';
+  }
+};
+
+var between = function between(x, min, max) {
+  return x >= min && x <= max;
+};
+
+textarea.addEventListener('input', changeCursor); //send button
 },{"../scss/main.scss":"scss/main.scss","./hover":"js/hover.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -225,7 +268,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54138" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53021" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
